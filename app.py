305de668b942880484a6cc65d001dfde5eb20dad
renebec <39104197+renebec@users.noreply.github.com>
2025-08-26 21:35:35 +0000

    mensaje de bienvenida

--- app.py
+++ app.py
@@ -190,7 +190,7 @@ def login():
                     session.permanent = True
                     session['username'] = username
                     session['last_activity'] = datetime.now().isoformat()
-                    flash(f'Bienvenido {username}', 'success')
+                    flash(f'{username} inició sesión', 'success')
                     return redirect(url_for('hello_pm1'))
                 else:
                     redirect(url_for('login.html'))
