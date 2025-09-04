b50fe36cbf5458df784b05d09099f314d636175b
renebec <39104197+renebec@users.noreply.github.com>
2025-09-04 14:42:56 +0000

    importando from werkzeug.utils import secure_filename

--- app.py
+++ app.py
@@ -11,6 +11,7 @@ import cloudinary.uploader
 import tempfile
 from weasyprint import HTML, CSS
 import pymysql
+from werkzeug.utils import secure_filename
 
 
 from database import load_pg_from_db, load_pgn_from_db,  register_user, get_db_session, insert_actividad, load_plan_from_db, insert_plan,  load_pg_from_db2
