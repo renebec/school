56167093c036a708d997c1ed9541424f77ab9a19
renebec <39104197+renebec@users.noreply.github.com>
2026-02-17 05:53:26 +0000

    s

--- app.py
+++ app.py
@@ -228,7 +228,7 @@ def enviaractividad():
                 return redirect(url_for("enviaractividad"))
 
             # --- Generar nombre único ---
-            base_name = f"actividad_{user['carrera']}_{user['apellido_paterno']}_{user['apellido_materno']}_{user['nombres']}_{actividad_num}_{asig}"
+            base_name = f"actividad_{user['carrera']}_{user['numero_control']}_{user['apellido_paterno']}_{user['apellido_materno']}_{user['nombres']}_{actividad_num}_{asig}"
             base_name = secure_filename(base_name)
             filename = f"{base_name}_{int(time.time())}.pdf"
 
