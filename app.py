3daf46137fbc14609f4e2539f267a33f685c2cc0
renebec <39104197+renebec@users.noreply.github.com>
2026-09-10 13:39:00 +0000

    import in app py patching monkey all

--- app.py
+++ app.py
@@ -13,6 +13,8 @@ from weasyprint import HTML, CSS
 import pymysql
 from werkzeug.utils import secure_filename
 from flask import send_file, abort
+from gevent import monkey
+monkey.patch_all()
 
 from database import load_pg_from_db, load_pgn_from_db,  register_user, get_db_session, insert_actividad, load_plan_from_db, insert_plan,  load_pg_from_db2, is_preregistered, load_user_pdfs, get_user_from_database, load_filtered_pdfs
 
